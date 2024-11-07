2f402c7b5c089463be3b041e0c0d8845298ab81f
Matthew Chandler <48606413+matthew-chandler@users.noreply.github.com>
2024-11-06 18:49:53 -0800

    moved enable/disable logic to popup

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-console.log("This is a popup!")
+let enabled = true;
+
+const enableButton = document.getElementById("enableButton");
+generatorButton.addEventListener("click", () => {enabled = true;});
+
+const generatorButton = document.getElementById("disableButton");
+generatorButton.addEventListener("click", () => {enabled = false;});
