b76783b57dae11dbdb0a9022eca3fef183384e68
Matthew Chandler <48606413+matthew-chandler@users.noreply.github.com>
2024-11-13 19:43:33 -0800

    updated buttons

--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
 let enabled = true;
 
 const enableButton = document.getElementById("enableButton");
-generatorButton.addEventListener("click", () => {enabled = true;});
+generatorButton.addEventListener("click", enableAds);
 
 const generatorButton = document.getElementById("disableButton");
-generatorButton.addEventListener("click", () => {enabled = false;});
+generatorButton.addEventListener("click", disableAds);
+
+enableAdsRuleset = {
+    "id": "ruleset_1",
+    "enabled": true,
+    "path": "rules.json"
+}
+
+disableAdsRuleset = {
+    "id": "ruleset_1",
+    "enabled": false,
+    "path": "rules.json"
+}
+
+function enableAds() {
+    let options = { enableRulesetsIds : enableAdsRuleset };
+    browser.declarativeNetRequest.updateEnabledRulesets({ enableRulesetIds : ["blocking_ruleset"], disableRulesetsIds : []});    
+}
+
+function disableAds() {
+    let options = { disableRulesetsIds : disableAdsRuleset };
+    browser.declarativeNetRequest.updateDEnabledRulesets({ enableRulesetIds : [], disableRulesetsIds : ["blocking_ruleset"]});    
+}
