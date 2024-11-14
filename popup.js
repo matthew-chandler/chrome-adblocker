93648c49aaa4b24a9aeb64873d0dbd16b4c2228d
Matthew Chandler <48606413+matthew-chandler@users.noreply.github.com>
2024-11-13 19:43:53 -0800

    removed unnecessary code

--- popup.js
+++ popup.js
@@ -6,18 +6,6 @@ generatorButton.addEventListener("click", enableAds);
 const generatorButton = document.getElementById("disableButton");
 generatorButton.addEventListener("click", disableAds);
 
-enableAdsRuleset = {
-    "id": "ruleset_1",
-    "enabled": true,
-    "path": "rules.json"
-}
-
-disableAdsRuleset = {
-    "id": "ruleset_1",
-    "enabled": false,
-    "path": "rules.json"
-}
-
 function enableAds() {
     let options = { enableRulesetsIds : enableAdsRuleset };
     browser.declarativeNetRequest.updateEnabledRulesets({ enableRulesetIds : ["blocking_ruleset"], disableRulesetsIds : []});    
